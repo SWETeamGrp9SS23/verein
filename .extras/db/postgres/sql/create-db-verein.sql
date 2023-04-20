5a336258f35e7a87c1ecbe7cb424e643eaf453cd
mischas114 <mischa.tettenborn@gmail.com>
2023-04-20 13:10:35 +0200

    Beschreibung db create

--- .extras/db/postgres/sql/create-db-verein.sql
+++ .extras/db/postgres/sql/create-db-verein.sql
@@ -1,3 +1,24 @@
+-- (1) user "postgres" in docker-compose.yaml auskommentieren,
+--     damit der PostgreSQL-Server implizit mit dem Linux-User "root" gestartet wird
+-- (2) PowerShell:
+--     cd <Verzeichnis-mit-docker-compose.yaml>
+--     docker compose up postgres
+-- (3) 2. PowerShell:
+--     cd <Verzeichnis-mit-docker-compose.yaml>
+--     docker compose exec postgres bash
+--         chown postgres:postgres /var/lib/postgresql/tablespace
+--         chown postgres:postgres /var/lib/postgresql/tablespace/verein
+--         exit
+--     docker compose down
+-- (3) in docker-compose.yaml den User "postgres" wieder aktivieren, d.h. Kommentar entfernen
+-- (4) 1. PowerShell:
+--     docker compose up
+-- (5) 2. PowerShell:
+--     docker compose exec postgres bash
+--        psql --dbname=postgres --username=postgres --file=/sql/create-db-verein.sql
+--        psql --dbname=verein --username=verein --file=/sql/create-schema-verein.sql
+--        exit
+--     docker compose down
 CREATE ROLE verein LOGIN PASSWORD 'p';
 CREATE DATABASE verein;
 GRANT ALL ON DATABASE verein TO verein;
