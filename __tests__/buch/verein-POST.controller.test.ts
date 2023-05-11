9d036d95bff7b79dc6110fff922c55f0a72d460d
vince <puvi1011@h-ka.de>
2023-05-11 09:06:03 +0200

    Post Controller test weiter angepasst

---
+++ __tests__/buch/verein-POST.controller.test.ts
@@ -0,0 +1,225 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { type VereinDTO } from '../../src/verein/rest/vereinDTO.entity.js';
+import { VereinReadService } from '../../src/verein/service/verein-read.service.js';
+import { HttpStatus } from '@nestjs/common';
+import { loginRest } from '../login.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const neuerVerein: VereinDTO = {
+    name: 'Fc Bayern',
+    mitgliedsbeitrag: 99.99,
+    entstehungsdatum: '2022-02-28',
+    homepage: 'https://post.rest',
+    adresse: {
+        ort: 'München',
+        plz: '80803',
+    },
+   
+};
+const neuesBuchInvalid: Record<string, unknown> = {
+    isbn: 'falsche-ISBN',
+    rating: -1,
+    art: 'UNSICHTBAR',
+    preis: -1,
+    rabatt: 2,
+    lieferbar: true,
+    datum: '12345-123-123',
+    homepage: 'anyHomepage',
+    titel: {
+        titel: '?!',
+        untertitel: 'Untertitelinvalid',
+    },
+};
+const neuerVereinIsbnExistiert: VereinDTO = {
+    isbn: '978-3-897-22583-1',
+    rating: 1,
+    art: 'DRUCKAUSGABE',
+    preis: 99.99,
+    rabatt: 0.099,
+    lieferbar: true,
+    datum: '2022-02-28',
+    homepage: 'https://post.isbn/',
+    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
+    titel: {
+        plz: 'Titelpostisbn',
+        ort: 'Untertitelpostisbn',
+    },
+    abbildungen: undefined,
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('POST /rest', () => {
+    let client: AxiosInstance;
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
+    };
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: (status) => status < 500, // eslint-disable-line @typescript-eslint/no-magic-numbers
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Neues Buch', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/rest',
+            neuerVerein,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.CREATED);
+
+        const { location } = response.headers as { location: string };
+
+        expect(location).toBeDefined();
+
+        // ObjectID: Muster von HEX-Ziffern
+        const indexLastSlash: number = location.lastIndexOf('/');
+
+        expect(indexLastSlash).not.toBe(-1);
+
+        const idStr = location.slice(indexLastSlash + 1);
+
+        expect(idStr).toBeDefined();
+        expect(VereinReadService.ID_PATTERN.test(idStr)).toBe(true);
+
+        expect(data).toBe('');
+    });
+
+    test('Neuen Verein mit ungueltigen Daten', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        const expectedMsg = [
+            expect.stringMatching(/^isbn /u),
+            expect.stringMatching(/^rating /u),
+            expect.stringMatching(/^art /u),
+            expect.stringMatching(/^preis /u),
+            expect.stringMatching(/^rabatt /u),
+            expect.stringMatching(/^datum /u),
+            expect.stringMatching(/^homepage /u),
+            expect.stringMatching(/^titel.titel /u),
+        ];
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuesBuchInvalid,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const messages: string[] = data.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    test('Neuer Verein, aber die ISBN existiert bereits', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/rest',
+            neuesVereinIsbnExistiert,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+        expect(data).toEqual(expect.stringContaining('ISBN'));
+    });
+
+    test('Neue Verein, aber ohne Token', async () => {
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerVerein,
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test('Neuer Verein, aber mit falschem Token', async () => {
+        // given
+        const token = 'FALSCH';
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerVerein,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test.todo('Abgelaufener Token');
+});
